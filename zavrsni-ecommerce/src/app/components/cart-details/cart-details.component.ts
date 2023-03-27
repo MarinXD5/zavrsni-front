d71fde4a77e7d6489c1044f9cea157d67eb1d9ef
Marin <marinjure.jurlina11@gmail.com>
2023-03-27 18:06:00 +0200

    change project main color to match logo

--- zavrsni-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ zavrsni-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -5,15 +5,14 @@ import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
-  styleUrls: ['./cart-details.component.css']
+  styleUrls: ['./cart-details.component.css'],
 })
 export class CartDetailsComponent implements OnInit {
-
   cartItems: CartItem[] = [];
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.listCartDetails();
@@ -22,27 +21,24 @@ export class CartDetailsComponent implements OnInit {
   listCartDetails() {
     this.cartItems = this.cartService.cartItems;
 
-    this.cartService.totalPrice.subscribe(
-      data => this.totalPrice = data
-    );
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
 
     this.cartService.totalQuantity.subscribe(
-      data => this.totalQuantity = data
+      (data) => (this.totalQuantity = data)
     );
 
     this.cartService.computeCartTotals();
   }
 
-  incrementQuantity(tempCartItem: CartItem){
+  incrementQuantity(tempCartItem: CartItem) {
     this.cartService.addToCart(tempCartItem);
   }
 
-  decrementQuantity(tempCartItem: CartItem){
+  decrementQuantity(tempCartItem: CartItem) {
     this.cartService.decrementQuantity(tempCartItem);
   }
 
-  remove(tempCartItem: CartItem){
+  remove(tempCartItem: CartItem) {
     this.cartService.remove(tempCartItem);
   }
-
 }
